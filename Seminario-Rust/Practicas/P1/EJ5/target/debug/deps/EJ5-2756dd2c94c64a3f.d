b24c94294924752ea41bfc3768d7507fb267c0c3
itsmateh <87345589+Matewh@users.noreply.github.com>
2025-03-28 22:19:31 -0300

    Rust

---
+++ Seminario-Rust/Practicas/P1/EJ5/target/debug/deps/EJ5-2756dd2c94c64a3f.d
@@ -0,0 +1,5 @@
+C:\Users\usuario\Desktop\Code\Informatica-UNLP\Cursadas\Seminario-Rust\Practicas\P1\EJ5\target\debug\deps\libEJ5-2756dd2c94c64a3f.rmeta: src\main.rs
+
+C:\Users\usuario\Desktop\Code\Informatica-UNLP\Cursadas\Seminario-Rust\Practicas\P1\EJ5\target\debug\deps\EJ5-2756dd2c94c64a3f.d: src\main.rs
+
+src\main.rs:
---
+++ Seminario-Rust/Practicas/P1/EJ5/target/debug/deps/EJ5-f0e3893cce1bc90b.d
@@ -0,0 +1,5 @@
+C:\Users\usuario\Desktop\Code\Informatica-UNLP\Cursadas\Seminario-Rust\Practicas\P1\EJ5\target\debug\deps\libEJ5-f0e3893cce1bc90b.rmeta: src\main.rs
+
+C:\Users\usuario\Desktop\Code\Informatica-UNLP\Cursadas\Seminario-Rust\Practicas\P1\EJ5\target\debug\deps\EJ5-f0e3893cce1bc90b.d: src\main.rs
+
+src\main.rs:
