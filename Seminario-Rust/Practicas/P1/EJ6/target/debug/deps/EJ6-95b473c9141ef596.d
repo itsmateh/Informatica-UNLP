b7e1247157d0c98c1bf173ab2ad994c15c13b328
itsmateh <87345589+Matewh@users.noreply.github.com>
2025-03-31 17:29:06 -0300

    Rust

---
+++ Seminario-Rust/Practicas/P1/EJ6/target/debug/deps/EJ6-95b473c9141ef596.d
@@ -0,0 +1,5 @@
+C:\Users\usuario\Desktop\Code\Informatica-UNLP\Cursadas\Seminario-Rust\Practicas\P1\EJ6\target\debug\deps\libEJ6-95b473c9141ef596.rmeta: src\main.rs
+
+C:\Users\usuario\Desktop\Code\Informatica-UNLP\Cursadas\Seminario-Rust\Practicas\P1\EJ6\target\debug\deps\EJ6-95b473c9141ef596.d: src\main.rs
+
+src\main.rs:
