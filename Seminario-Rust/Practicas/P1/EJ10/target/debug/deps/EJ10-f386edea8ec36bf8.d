4ceb36d866a288d595e09f6c7d2c6f6c390e59fb
itsmateh <87345589+Matewh@users.noreply.github.com>
2025-03-31 18:38:39 -0300

    Rust

---
+++ Seminario-Rust/Practicas/P1/EJ10/target/debug/deps/EJ10-f386edea8ec36bf8.d
@@ -0,0 +1,5 @@
+C:\Users\usuario\Desktop\Code\Informatica-UNLP\Cursadas\Seminario-Rust\Practicas\P1\EJ10\target\debug\deps\libEJ10-f386edea8ec36bf8.rmeta: src\main.rs
+
+C:\Users\usuario\Desktop\Code\Informatica-UNLP\Cursadas\Seminario-Rust\Practicas\P1\EJ10\target\debug\deps\EJ10-f386edea8ec36bf8.d: src\main.rs
+
+src\main.rs:
